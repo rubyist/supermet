b3ad41a4248ca6d31331e7481b4c1e096b447f2e
Scott Barron <scott@elitists.net>
2009-02-16 18:16:08 -0500

    setup

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SuperMet
+//
+//  Created by Scott Barron on 2/16/09.
+//  Copyright Scott Barron 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
